2a0267ecfd9330335573d40265bb7726d2185592
Greta Wall <greta.wall@kellogg.northwestern.edu>
2024-06-01 17:33:44 +0000

    changes

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,13 +4,18 @@ class EntriesController < ApplicationController
   end
 
   def create
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry.save
-    redirect_to "/places/#{@entry["place_id"]}"
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user != nil
+      @post = Post.new
+      @post["body"] = params["body"]
+      @post["image"] = params["image"]
+      @post.uploaded_image.attach(params["uploaded_image"])
+      @post["user_id"] = @user["id"]
+      @post.save
+    else
+      flash["notice"] = "Login first."
+    end
+    redirect_to "/posts"
   end
 
 end
